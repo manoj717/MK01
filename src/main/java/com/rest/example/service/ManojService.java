1152a189017e4d6b74be86f563393ffa4247ce21
Manoj Kumar G <manojkumarkumara11@gmail.com>
2024-03-30 19:26:42 +0530

    initial Commit

--- src/main/java/com/rest/example/service/ManojService.java
+++ src/main/java/com/rest/example/service/ManojService.java
@@ -1,4 +1,8 @@
 package com.rest.example.service;
 
+import com.rest.example.DTO.APIResonseDTO;
+
 public interface ManojService {
+
+    public APIResonseDTO getResponse();
 }
--- src/main/java/com/rest/example/service/ManojServiceImpl.java
+++ src/main/java/com/rest/example/service/ManojServiceImpl.java
@@ -1,4 +1,10 @@
 package com.rest.example.service;
 
+import com.rest.example.DTO.APIResonseDTO;
+
 public class ManojServiceImpl implements ManojService {
+    @Override
+    public APIResonseDTO getResponse() {
+        return null;
+    }
 }
