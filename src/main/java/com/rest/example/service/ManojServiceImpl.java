d8d60930158b21a6bbeb1fd224d607898989942e
Manoj Kumar G <manojkumarkumara11@gmail.com>
2024-03-30 19:31:07 +0530

    initial Commit

--- src/main/java/com/rest/example/service/ManojServiceImpl.java
+++ src/main/java/com/rest/example/service/ManojServiceImpl.java
@@ -1,7 +1,9 @@
 package com.rest.example.service;
 
 import com.rest.example.DTO.APIResonseDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ManojServiceImpl implements ManojService {
     @Override
     public APIResonseDTO getResponse() {
