c30f1ac930d7fc8f911d4a9495da654cc4d0542f
Manoj Kumar G <manojkumarkumara11@gmail.com>
2024-03-30 19:38:30 +0530

    initial Commit

--- src/main/java/com/rest/example/controller/ManojController.java
+++ src/main/java/com/rest/example/controller/ManojController.java
@@ -1,7 +1,13 @@
 package com.rest.example.controller;
 
+import com.rest.example.DTO.APIResonseDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ManojController {
+
+    private ResponseEntity<APIResonseDTO> getResponse(){
+        return ResponseEntity.ok().body(new APIResonseDTO());
+    }
 }
